6ed806ddc3ae77fcb1c3d5a34c566a8f302b87aa
Nitza Alfinas <nitzaalfinas@gmail.com>
2019-08-15 05:04:12 +0700

    rm cache

--- env.rb
+++
@@ -1,4 +0,0 @@
-MINE = 0
-KEYSTORE_PATH = "/Users/nitzaalfinas/Documents/app_group/blockchain/nitchain_user/data/user/keystore"
-MAIN_CONFIG_PATH = "/Users/nitzaalfinas/Documents/app_group/blockchain/nitchain_user/data/config"
-MAIN_POOL = 'http://localhost:4568'
