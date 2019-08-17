5ffc9a88b505c6c3b88f203f2a3a2445b60f221b
Nitza Alfinas <nitzaalfinas@gmail.com>
2019-08-17 08:41:01 +0700

    information about merkle_boy.rb file

--- merkle_boy.rb
+++ merkle_boy.rb
@@ -1,3 +1,6 @@
+# File ini dibuat hanya untuk keperluan riset sederhana cara membuat merkle root
+# Setelah berhasil implementasi, file ini akan dihapus
+
 merkles_arr = ["1","2","3","4","5","6","7","8","9","10","11","12","13","14"]
 
 def jalan(merkles_arr)
