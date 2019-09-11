417270aea242c159bc9668fb59938ce620b526aa
Nitza Alfinas <nitzaalfinas@gmail.com>
2019-09-11 23:06:08 +0700

    transaction format

--- controller/transaction.rb
+++ controller/transaction.rb
@@ -12,8 +12,33 @@ class Transaction
     ##
     # == Keterangan
     # Creating transaction from json data
+    #
     # == Params
     # tx = hash
+    #
+    # == Return
+    # {
+    #     :success=>true,
+    #     :data=>{
+    #         :hash=>"0b535fb545fed23e4efc61a7e9185f11bade1a57504a64cec26d47aa2d65becd",
+    #         :pubkey=>"-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAvwunW9qbYH+KnXpxEu8w\niLiZSN2s1o21z7KG9w8k+xsbTYJ+BfGPEYKnP34eKN+5pp7lo+Uvfd/NIeO2c/gs\nzsc9iZ3mjuJAxdMArirpptac5bdR77/jSmL6hws1uuvaX+SC/5LziwjbSOZn/xl7\nBlsYMRmreVJJyTbBoMeewYXYJ+zBKBsJjo/nHf1cRrlB2nMX1IahW7uE7nJaVT72\nvdCMR1Dq418StJX6hN8qG3xR6f1KWtTHqKl2Ykdi+l6pKYK8GOO+3RpRGadvDluo\nIfcQGdcE3IsRWYwrReMIt/GgjfhxNIi1nac6+ASrNtMv2UAX567h7mMeliJ4fO5j\ngwIDAQAB\n-----END PUBLIC KEY-----\n",
+    #         :sign=>"bXagljovmQQ6urDK+9CVxWFLjk8ghD/Z5kmrHPA0GsdR4twU3MHd0Zuyk+M/\ncRwI2BQ5k+3xyDzvWTisKYKWWisk4PfDU0+em3Qi7Q8NsdTNl43wrJLNi5JG\nWS6WXDN51xjlVdAKx3Ko/zwaqpVozT4fGGR9vCHGDCuYH1frphEs59bB5zz5\nWiTQMCfTiUpuPG9AY0HcgzxIHS1mnYRuxArhTEXGScyyRQaI1AcLybo/u7uB\nm1I7gWmp1mK3hCnINGle17cB0gf4/+ZLf0mcPD2B6nq+p2lWSlOHAf87jNRP\nGmKVmVLPXqpee+jLSXeYhZ19n87qg5nK80WLk9kAlg==\n",
+    #         :tx=>{
+    #             :input=>{
+    #                 :balance=>1000000050,
+    #                 :from=>"Nxf154127e23cde0c8ecbaa8b943aff970c60c590f",
+    #                 :to=>"Nxf9c62974d550c1f12cd7d6b9913b44983cb3a096",
+    #                 :amount=>100,
+    #                 :fee=>5
+    #             },
+    #             :outputs=>[
+    #                 {:address=>"Nxf154127e23cde0c8ecbaa8b943aff970c60c590f", :balance=>999999945},
+    #                 {:address=>"Nxf9c62974d550c1f12cd7d6b9913b44983cb3a096", :balance=>100}
+    #             ],
+    #             :time=>1568215686
+    #         }
+    #     }
+    # }
     def self.create(datainput)
         data = JSON.parse(datainput)
 
@@ -86,34 +111,31 @@ class Transaction
         end
     end
 
+    def self.send_to_pool(transaction_data)
+
+    end
+
     ##
     # == Params
     # txdata = data for a transaction form key-value hash class
     # == Data structure
     # {
-    #     "hash" : "9c81dff5bcdc7909b7e3f042280583e36ab8073e3146ae70046b6f338a6dbb7a",
-    #     "from" : "Nxf154127e23cde0c8ecbaa8b943aff970c60c590f",
-    #     "to" : "Nxf9c62974d550c1f12cd7d6b9913b44983cb3a096",
-    #     "pubkey" : "-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAwylo6ELYbXFVPmveYEsa\nBvHx3dPpFWmubdlMqFLAVluDY/+maG8CsXFO3GDTAK7z8qj8ZggOIWAhWW0VkDWl\ntbkZezfvloQnbV9lwMncCEQX2YEAK0cl8JmUTXmDkeeRBGZNj7VHa+/6uMv0HZGm\ns5sKr51TGIoxlnH4s8jLkd83mUcLQz+9/MXdMMS8lcNengk7ZTs/xA+PgRozf/yV\n/hc8Qf6DdMxYYmg4rEgZjuO1TghJ9o5QJXYUYZDr40he39ZUJDw/11PKnQQcJNaO\ncv+iQfTtFAGHHo/eBFFzjNccYm8/ojnGGGORlYzH9OXiA4wVG0Z/BNdtl5Wi/xvP\nLQIDAQAB\n-----END PUBLIC KEY-----\n",
-    #     "sign" : "CeTOJSk3tCqwFTza+a9YEhaaJ9yIN/HVhdy/GmMONMg1CiN7yVggsviGo+t1\n2sK1G7dW4siCloJJIjHJbvvrmAeeI+UvK3YkWdAFbPd3uDEhj2zIAPnvMWIJ\n7/RoAYmGJtItiAn9Qznb17tAzEpg28JCMXNulzgdbdfYfB9lau241UPjzwUC\nubWhp9t/u3PAXgVmLuXhD2+JvGOaty4aKX2B/jkuY549/N2lbO2rM31CtNtN\nyBqtrUPUCyMadguueo+k6KxMfQaowb8Jwulc79GaIJzVOjHuFPBpFPRuwTQT\n1d+xnvxlt8jl+Zn0HMxCSr0Ctkr/taktqaGtatOUew==\n",
-    #     "tx" : {
-    #         "input" : {
-    #             "from" : "Nxf154127e23cde0c8ecbaa8b943aff970c60c590f",
-    #             "balance" : 1000000050,
-    #             "to" : "Nxf9c62974d550c1f12cd7d6b9913b44983cb3a096",
-    #             "amount" : 100000
+    #     :hash=>"0b535fb545fed23e4efc61a7e9185f11bade1a57504a64cec26d47aa2d65becd",
+    #     :pubkey=>"-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAvwunW9qbYH+KnXpxEu8w\niLiZSN2s1o21z7KG9w8k+xsbTYJ+BfGPEYKnP34eKN+5pp7lo+Uvfd/NIeO2c/gs\nzsc9iZ3mjuJAxdMArirpptac5bdR77/jSmL6hws1uuvaX+SC/5LziwjbSOZn/xl7\nBlsYMRmreVJJyTbBoMeewYXYJ+zBKBsJjo/nHf1cRrlB2nMX1IahW7uE7nJaVT72\nvdCMR1Dq418StJX6hN8qG3xR6f1KWtTHqKl2Ykdi+l6pKYK8GOO+3RpRGadvDluo\nIfcQGdcE3IsRWYwrReMIt/GgjfhxNIi1nac6+ASrNtMv2UAX567h7mMeliJ4fO5j\ngwIDAQAB\n-----END PUBLIC KEY-----\n",
+    #     :sign=>"bXagljovmQQ6urDK+9CVxWFLjk8ghD/Z5kmrHPA0GsdR4twU3MHd0Zuyk+M/\ncRwI2BQ5k+3xyDzvWTisKYKWWisk4PfDU0+em3Qi7Q8NsdTNl43wrJLNi5JG\nWS6WXDN51xjlVdAKx3Ko/zwaqpVozT4fGGR9vCHGDCuYH1frphEs59bB5zz5\nWiTQMCfTiUpuPG9AY0HcgzxIHS1mnYRuxArhTEXGScyyRQaI1AcLybo/u7uB\nm1I7gWmp1mK3hCnINGle17cB0gf4/+ZLf0mcPD2B6nq+p2lWSlOHAf87jNRP\nGmKVmVLPXqpee+jLSXeYhZ19n87qg5nK80WLk9kAlg==\n",
+    #     :tx=>{
+    #         :input=>{
+    #             :balance=>1000000050,
+    #             :from=>"Nxf154127e23cde0c8ecbaa8b943aff970c60c590f",
+    #             :to=>"Nxf9c62974d550c1f12cd7d6b9913b44983cb3a096",
+    #             :amount=>100,
+    #             :fee=>5
     #         },
-    #         "outputs" : [
-    #             {
-    #                 "address" : "Nxf154127e23cde0c8ecbaa8b943aff970c60c590f",
-    #                 "balance" : 999900050
-    #             },
-    #             {
-    #                 "address" : "Nxf9c62974d550c1f12cd7d6b9913b44983cb3a096",
-    #                 "balance" : 100000
-    #             }
+    #         :outputs=>[
+    #             {:address=>"Nxf154127e23cde0c8ecbaa8b943aff970c60c590f", :balance=>999999945},
+    #             {:address=>"Nxf9c62974d550c1f12cd7d6b9913b44983cb3a096", :balance=>100}
     #         ],
-    #         "time" : 1567752548
+    #         :time=>1568215686
     #     }
     # }
     def self.validation(txdata)
