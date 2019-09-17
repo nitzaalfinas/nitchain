9ca48b94dc1e5ef75257b16b977a6e5418d0f66a
Nitza Alfinas <nitzaalfinas@gmail.com>
2019-09-17 13:46:36 +0700

    hapus data code dummy

--- mytest/controller/test_block.rb
+++ mytest/controller/test_block.rb
@@ -83,16 +83,6 @@ class TestBlock < Test::Unit::TestCase
         return data
     end
 
-    # test "not test, just want to create tx" do
-    #     datasend = {
-    #         from: "Nxf9c62974d550c1f12cd7d6b9913b44983cb3a096",
-    #         to: "Nxf154127e23cde0c8ecbaa8b943aff970c60c590f",
-    #         amount: 95,
-    #         fee: 5
-    #     }
-    #     puts Transaction.create(datasend.to_json).to_json
-    # end
-
     test "valid genesis" do
         block = '{"hash":"000041e532fb80cab55811483e5db2933bb315ecacda5cff7892dccde9a05985","data":{"num":1,"prevhash":"","tcount":1,"tamount":1000000050,"diff":3,"mroot":"72000f400172a33b7f014d63eb0d0e7a29a787a53f085acbf40163fe028fb077","miner":"Nxf9c62974d550c1f12cd7d6b9913b44983cb3a096","reward":50,"txs":["2790af614ddc0b188bd8b7dd8f9c2e4bbf98c6abdb7ef38a9d987c3d8f228bf7"],"txds":[{"hash":"2790af614ddc0b188bd8b7dd8f9c2e4bbf98c6abdb7ef38a9d987c3d8f228bf7","pubkey":"-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAvwunW9qbYH+KnXpxEu8w\niLiZSN2s1o21z7KG9w8k+xsbTYJ+BfGPEYKnP34eKN+5pp7lo+Uvfd/NIeO2c/gs\nzsc9iZ3mjuJAxdMArirpptac5bdR77/jSmL6hws1uuvaX+SC/5LziwjbSOZn/xl7\nBlsYMRmreVJJyTbBoMeewYXYJ+zBKBsJjo/nHf1cRrlB2nMX1IahW7uE7nJaVT72\nvdCMR1Dq418StJX6hN8qG3xR6f1KWtTHqKl2Ykdi+l6pKYK8GOO+3RpRGadvDluo\nIfcQGdcE3IsRWYwrReMIt/GgjfhxNIi1nac6+ASrNtMv2UAX567h7mMeliJ4fO5j\ngwIDAQAB\n-----END PUBLIC KEY-----\n","sign":"GNrakdr3AbvUfFLn0gmYBD8kAwk7cmjwOXkt3XbhlyS1iYzgDuKG31WMaOaZ\nafgFETIfDG2ixxi5luwbwu6TTVFNUFrWFpGid/6zi88/0tewCqJbyzz2Rf+q\nj0L8X9oPPVo5RHxSF5wQHA04XH4xfYCVABCKf3/zLDtdOLaCd78hxG1KAoDU\nIbZw9M8ENqzKPVAt0zFCEcrgJJTYTfk5GZxZLoE6i3zn0CLdwkToHDiHJVKe\neiPkfQao8Lu0QKfJ5EPqhszNR2GiqJ304aEZuMjF8yPGlGERysYI58kmYnGO\nBXZ36z2BRERsjPcjPYllMrb6xoOaX0mZFEFt8RQXlQ==\n","tx":{"input":{"genesis":true,"from":"Nxf154127e23cde0c8ecbaa8b943aff970c60c590f","balance":1000000000,"to":"Nxf9c62974d550c1f12cd7d6b9913b44983cb3a096","amount":1000000000,"fee":0,"data":{}},"outputs":[{"address":"Nxf154127e23cde0c8ecbaa8b943aff970c60c590f","balance":0},{"address":"Nxf9c62974d550c1f12cd7d6b9913b44983cb3a096","balance":1000000000},{"address":"miner","balance":0}],"time":1567676889}}],"outputs":[{"address":"Nxf154127e23cde0c8ecbaa8b943aff970c60c590f","balance":0},{"address":"Nxf9c62974d550c1f12cd7d6b9913b44983cb3a096","balance":1000000050}],"time":1567743389,"nonce":6320}}'
 
