701c9a6c6284b1ac7abbf376bfbabc8def4532c5
Nitza Alfinas <nitzaalfinas@gmail.com>
2019-09-18 20:12:21 +0700

    test transaction selesai

--- mytest/controller/test_transaction.rb
+++ mytest/controller/test_transaction.rb
@@ -5,6 +5,8 @@ require 'awesome_print'
 
 ENV = "test"
 
+require_relative "setup_test"
+
 require_relative '../../controller/wallet'
 require_relative '../../controller/transaction'
 
@@ -28,35 +30,43 @@ class TestTransaction < Test::Unit::TestCase
     test "def validation" do
         def data_start
             data_to_validate = {
-                "hash":"f45a614f0725d9981182735af505af71e02bfa0c56958ba648dd9cee9b63eb5a",
-                "pubkey":"-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAvwunW9qbYH+KnXpxEu8w\niLiZSN2s1o21z7KG9w8k+xsbTYJ+BfGPEYKnP34eKN+5pp7lo+Uvfd/NIeO2c/gs\nzsc9iZ3mjuJAxdMArirpptac5bdR77/jSmL6hws1uuvaX+SC/5LziwjbSOZn/xl7\nBlsYMRmreVJJyTbBoMeewYXYJ+zBKBsJjo/nHf1cRrlB2nMX1IahW7uE7nJaVT72\nvdCMR1Dq418StJX6hN8qG3xR6f1KWtTHqKl2Ykdi+l6pKYK8GOO+3RpRGadvDluo\nIfcQGdcE3IsRWYwrReMIt/GgjfhxNIi1nac6+ASrNtMv2UAX567h7mMeliJ4fO5j\ngwIDAQAB\n-----END PUBLIC KEY-----\n",
-                "sign":"blDZiWejsa/B6EKWS4LBgXxsZi/LYFxJ0uQ+NvNKL1GhhT6BpxxEB7HyVTAd\n7EvRumkKofeHzQBwJdFLeNwGfpBjvTfi2S1lDOU0gSHZdFPOUDWyDfZl56f5\nHhJaSS0F1L/mEER1qUVwSqTVIFsUSEwDfcN6pnMsQO61QjYTKAAKngFevy3+\nQalEDh3xxSWd/47i11tn2Gr4plFKzAjPJNoUrOvzVrPXbRjFw3oUrKBqGO1T\nJCeQ0nhbjIU5DtMExdNnjXP2l7OPB56VziXRGNC0kMMp394WYIKKJV+6rz2i\nslA6O816o5Q3DYqmXJHTQ4mVqds8LEWxmVmNacTxsg==\n",
-                "tx":{
-                    "input":{
-                        "balance":1000000050,
-                        "from":"Nxf154127e23cde0c8ecbaa8b943aff970c60c590f",
-                        "to":"Nxf9c62974d550c1f12cd7d6b9913b44983cb3a096",
-                        "amount":100,
-                        "fee":5,
-                        "data":{}
+                "hash": "4baf6c21c4611822971ee5c7354f3979cdb4c13b1a8972fefd04b4e0aa839730",
+                "pubkey": "-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAwylo6ELYbXFVPmveYEsa\nBvHx3dPpFWmubdlMqFLAVluDY/+maG8CsXFO3GDTAK7z8qj8ZggOIWAhWW0VkDWl\ntbkZezfvloQnbV9lwMncCEQX2YEAK0cl8JmUTXmDkeeRBGZNj7VHa+/6uMv0HZGm\ns5sKr51TGIoxlnH4s8jLkd83mUcLQz+9/MXdMMS8lcNengk7ZTs/xA+PgRozf/yV\n/hc8Qf6DdMxYYmg4rEgZjuO1TghJ9o5QJXYUYZDr40he39ZUJDw/11PKnQQcJNaO\ncv+iQfTtFAGHHo/eBFFzjNccYm8/ojnGGGORlYzH9OXiA4wVG0Z/BNdtl5Wi/xvP\nLQIDAQAB\n-----END PUBLIC KEY-----\n",
+                "sign": "dV+b+7gArDPoadFe1PZv3CjOCgyhBGNoBr6N8qFcLrZsDIMuR3z/qM1xKDiS\nbvOeeRgtKjeKpkg55nvoctCoilpTuccEV6BHSsQuQB6zDiUb601p/L0JfJhK\nWj7eUQejYj+nnPAZNzG39o362pjWmqscgT15Az1avVji0Fz4AL+kAuRt5h4d\nIejBO61CtcjCO72VFMJqmmTYvpYUSojOJ5oN0WJ/r5mdbMVmpiD4ORh2z9BA\nONgy1tlVH4r/fnDfKmfoTW7rkYdM/sqVSyHmsNK+NpX5OsfqcMz3B9TLMyjC\niYdhwJMKZ/2jkEaIa/VUZSCBj0w7qEe3yZfF/kKyoA==\n",
+                "tx": {
+                    "input": {
+                        "from": "Nxf9c62974d550c1f12cd7d6b9913b44983cb3a096",
+                        "balance": 1000000055,
+                        "to": "Nxf154127e23cde0c8ecbaa8b943aff970c60c590f",
+                        "amount": 95,
+                        "fee": 5,
+                        "data": {}
                     },
-                    "outputs":[
-                        {"address":"Nxf154127e23cde0c8ecbaa8b943aff970c60c590f","balance":999999945},
-                        {"address":"Nxf9c62974d550c1f12cd7d6b9913b44983cb3a096","balance":100}
+                    "outputs": [
+                        {
+                            "address": "Nxf9c62974d550c1f12cd7d6b9913b44983cb3a096",
+                            "balance": 999999955
+                        },
+                        {
+                            "address": "Nxf154127e23cde0c8ecbaa8b943aff970c60c590f",
+                            "balance": 140
+                        },
+                        {
+                            "address": "miner",
+                            "balance": 5
+                        }
                     ],
-                    "time":1568218748
+                    "time": 1568695754
                 }
             }
             return data_to_validate
         end
 
-        # --- false "invalid format"---
-        # hapus sign
-        data = data_start
-        data.delete(:sign)
-        data_hash_key_string = JSON.parse(data.to_json)
-        assert_equal(false, Transaction.validation(data_hash_key_string)[:success])
-        # --- false ---
+        # ------------
+        # karena transaksi ini harus diletakkan pada block nomor 3, maka kita harus hapus semua data dan berikan seed untuk populasi database jadi block 1 dan 2
+        SetupTest.destroy_blockchain_collections
+        SetupTest.seed_db
+        # ------------
 
         # --- false "invalid format"---
         # hapus sign
