f97530c71beeeb97dc500e2441973d402bd67225
Nitza Alfinas <nitzaalfinas@gmail.com>
2019-09-20 05:41:14 +0700

    new pool

--- mytest/controller/setup_test.rb
+++ mytest/controller/setup_test.rb
@@ -7,54 +7,36 @@ class SetupTest
     end
 
 
-
     def self.seed_blockchains_collections
 
         client = Mongo::Client.new([ '127.0.0.1:27017' ], :database => DATABASE_NAME)
 
         block_genesis = {
-            "hash": "000041e532fb80cab55811483e5db2933bb315ecacda5cff7892dccde9a05985",
+            "hash": "000b25a03fe29683fa84e2b4a9771c7813e44d73001b984b08f9cd580289dbfd",
             "data": {
                 "num": 1,
                 "prevhash": "",
                 "tcount": 1,
                 "tamount": 1000000050,
                 "diff": 3,
-                "mroot": "72000f400172a33b7f014d63eb0d0e7a29a787a53f085acbf40163fe028fb077",
+                "mroot": "fca28e46e38245195a286f4f1788046db49564ba203df4eefa823544426b01aa",
                 "miner": "Nxf9c62974d550c1f12cd7d6b9913b44983cb3a096",
                 "reward": 50,
+                "tfee": 0,
                 "txs": [
-                    "2790af614ddc0b188bd8b7dd8f9c2e4bbf98c6abdb7ef38a9d987c3d8f228bf7"
+                    "369a65796e82c2bd89175d7afe0c06dc387ee09c955687acad0b76d0ef17a5d7"
                 ],
                 "txds": [
                     {
-                        "hash": "2790af614ddc0b188bd8b7dd8f9c2e4bbf98c6abdb7ef38a9d987c3d8f228bf7",
+                        "hash": "369a65796e82c2bd89175d7afe0c06dc387ee09c955687acad0b76d0ef17a5d7",
                         "pubkey": "-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAvwunW9qbYH+KnXpxEu8w\niLiZSN2s1o21z7KG9w8k+xsbTYJ+BfGPEYKnP34eKN+5pp7lo+Uvfd/NIeO2c/gs\nzsc9iZ3mjuJAxdMArirpptac5bdR77/jSmL6hws1uuvaX+SC/5LziwjbSOZn/xl7\nBlsYMRmreVJJyTbBoMeewYXYJ+zBKBsJjo/nHf1cRrlB2nMX1IahW7uE7nJaVT72\nvdCMR1Dq418StJX6hN8qG3xR6f1KWtTHqKl2Ykdi+l6pKYK8GOO+3RpRGadvDluo\nIfcQGdcE3IsRWYwrReMIt/GgjfhxNIi1nac6+ASrNtMv2UAX567h7mMeliJ4fO5j\ngwIDAQAB\n-----END PUBLIC KEY-----\n",
-                        "sign": "GNrakdr3AbvUfFLn0gmYBD8kAwk7cmjwOXkt3XbhlyS1iYzgDuKG31WMaOaZ\nafgFETIfDG2ixxi5luwbwu6TTVFNUFrWFpGid/6zi88/0tewCqJbyzz2Rf+q\nj0L8X9oPPVo5RHxSF5wQHA04XH4xfYCVABCKf3/zLDtdOLaCd78hxG1KAoDU\nIbZw9M8ENqzKPVAt0zFCEcrgJJTYTfk5GZxZLoE6i3zn0CLdwkToHDiHJVKe\neiPkfQao8Lu0QKfJ5EPqhszNR2GiqJ304aEZuMjF8yPGlGERysYI58kmYnGO\nBXZ36z2BRERsjPcjPYllMrb6xoOaX0mZFEFt8RQXlQ==\n",
+                        "sign": "kcfvCAl8aJs3YFa7fK2BdY2RqpJ9FepcA2s3rfY5/CTWnQBqhnSeT5x90ak7\nswDQZkJdSyhx4In/Ly38ttxrvPESwnYL7Fr2TGTuyW3REdlnzXxefM+f7D87\nmKYl4GlYbfsj4julHMss4McKB6As5ekW24cHVJ1/qSAmzHykHU25K5JuLCk9\nLb6sarbPsWkhEhwnAPLag2b3rm1GmPZbzjB947avulS17ubTfdHtGN5Q1lkG\nLXnSz8szy0NXAaB0y2q6tEQI28NrYDXfzXd5Oc8U7GwF2SIASeaBatfGTcJG\nFQeyqWYHGEOrdaNtgkhjipwHR3HfZZeMvue8YQ2AXA==\n",
                         "tx": {
-                            "input": {
-                                "genesis": true,
-                                "from": "Nxf154127e23cde0c8ecbaa8b943aff970c60c590f",
-                                "balance": 1000000000,
-                                "to": "Nxf9c62974d550c1f12cd7d6b9913b44983cb3a096",
-                                "amount": 1000000000,
-                                "fee": 0,
-                                "data": {}
-                            },
-                            "outputs": [
-                                {
-                                    "address": "Nxf154127e23cde0c8ecbaa8b943aff970c60c590f",
-                                    "balance": 0
-                                },
-                                {
-                                    "address": "Nxf9c62974d550c1f12cd7d6b9913b44983cb3a096",
-                                    "balance": 1000000000
-                                },
-                                {
-                                    "address": "miner",
-                                    "balance": 0
-                                }
-                            ],
+                            "from": "Nxf154127e23cde0c8ecbaa8b943aff970c60c590f",
+                            "to": "Nxf9c62974d550c1f12cd7d6b9913b44983cb3a096",
+                            "amount": 1000000000,
+                            "fee": 0,
+                            "data": {},
                             "time": 1567676889
                         }
                     }
@@ -69,69 +51,53 @@ class SetupTest
                         "balance": 1000000050
                     }
                 ],
-                "time": 1567743389,
-                "nonce": 6320
+                "time": 1568901319,
+                "nonce": 5321
             }
         }
 
         block_second = {
-            "hash": "000111b142250f0275bd2e18549b358d957837f96d63883fec22458228adfbcd",
+            "hash": "000a26c655b384a82ada529033aa893c7db0808176affeca42e5a0f4433264d4",
             "data": {
                 "num": 2,
-                "prevhash": "000041e532fb80cab55811483e5db2933bb315ecacda5cff7892dccde9a05985",
+                "prevhash": "00042bc278082c550e9a6ad9aa799211145e6c92385a194fb09c5d57f12897d0",
                 "tcount": 1,
                 "tamount": 1000000100,
                 "diff": 3,
-                "mroot": "ec0dbe032e88fa0f64b103453419d62d29e4ecbef27d7e983764df51bf5ad3ee",
+                "mroot": "9513b08f0301a13b425d8e9463e4cc5c24a5272a7440291c05cb3623a8a13459",
                 "miner": "Nxf9c62974d550c1f12cd7d6b9913b44983cb3a096",
                 "reward": 50,
+                "tfee": 5,
                 "txs": [
-                    "4e90136b21c89e24bc9fe65eaa2dfe071e38a719201ef88f49d714609c4d949f"
+                    "05cb8c45ebbdb4b4cc4af3569e7366711d03cbb209cf860d2bb12845b734d2a1"
                 ],
                 "txds": [
                     {
-                        "hash": "4e90136b21c89e24bc9fe65eaa2dfe071e38a719201ef88f49d714609c4d949f",
-                        "pubkey": "-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAwylo6ELYbXFVPmveYEsa\nBvHx3dPpFWmubdlMqFLAVluDY/+maG8CsXFO3GDTAK7z8qj8ZggOIWAhWW0VkDWl\ntbkZezfvloQnbV9lwMncCEQX2YEAK0cl8JmUTXmDkeeRBGZNj7VHa+/6uMv0HZGm\ns5sKr51TGIoxlnH4s8jLkd83mUcLQz+9/MXdMMS8lcNengk7ZTs/xA+PgRozf/yV\n/hc8Qf6DdMxYYmg4rEgZjuO1TghJ9o5QJXYUYZDr40he39ZUJDw/11PKnQQcJNaO\ncv+iQfTtFAGHHo/eBFFzjNccYm8/ojnGGGORlYzH9OXiA4wVG0Z/BNdtl5Wi/xvP\nLQIDAQAB\n-----END PUBLIC KEY-----\n",
-                        "sign": "TeOANnD6AuZHpT+R35OZxbE1LR7/vVkjwM3kmU8ezefOGqFkRgwJofSXtwYx\nSwToCJmgRtm/qwcIrU0Rqoln3yxG4L2WXG/Kxszdp8zdRkHRydrA5rtbo0m8\nYPtOaFGCXcg7AtsxE2VclnGPhyIAow0kmOw/3ola/ZpdOClS6mw7P0eTSAcQ\n/gqV/R+p7Ceq7cYCBVohBjUVWIYar+iAGNeYqmTJSftc9EuK8fD9Odc4prHW\nLPqoVulwdkZDmrYNpHgT/27xQNZE53la+AeEIq1WZuCgENk1G0rV7qSgqT3Q\nvE1Vg3ujY0qRICfFIS7y1Z6/ZYB9Ba97i3LCguvUDQ==\n",
+                        "hash": "05cb8c45ebbdb4b4cc4af3569e7366711d03cbb209cf860d2bb12845b734d2a1",
+                        "pubkey": "-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAvwunW9qbYH+KnXpxEu8w\niLiZSN2s1o21z7KG9w8k+xsbTYJ+BfGPEYKnP34eKN+5pp7lo+Uvfd/NIeO2c/gs\nzsc9iZ3mjuJAxdMArirpptac5bdR77/jSmL6hws1uuvaX+SC/5LziwjbSOZn/xl7\nBlsYMRmreVJJyTbBoMeewYXYJ+zBKBsJjo/nHf1cRrlB2nMX1IahW7uE7nJaVT72\nvdCMR1Dq418StJX6hN8qG3xR6f1KWtTHqKl2Ykdi+l6pKYK8GOO+3RpRGadvDluo\nIfcQGdcE3IsRWYwrReMIt/GgjfhxNIi1nac6+ASrNtMv2UAX567h7mMeliJ4fO5j\ngwIDAQAB\n-----END PUBLIC KEY-----\n",
+                        "sign": "GpUFvFbcXoV3SkvogXhTKnBvWgzPMHF/N49ZWHkdCyL01RVdfu7DslgGrEpJ\nwxqRgQfp0eLuSNiqKLJUodHkTAzeLE7s129AC8Y0Lv82gpgQ1Q2mz8zhYf1e\nI+FhMSbdl3u+p6mOnDke0VdF30dSGtpRSmK2INnSZiF1+AMCcmMdvf77Sxc+\n+s5JsfN8JQD9h7tIL+15juBGugwFexkg+C7u2B+v2YAR5LdCjdookv6HcTgv\ny4nozvLDATkQj9QmTseXrKOfJ2hbGcZ/YSrjQYvqX/LqSfEaTDu9Sq5Mqk0h\nLMIOs7Yxjehg4fCVyqSn4oV98ThyPQvbr04Vdjxokw==\n",
                         "tx": {
-                            "input": {
-                                "from": "Nxf9c62974d550c1f12cd7d6b9913b44983cb3a096",
-                                "balance": 1000000050,
-                                "to": "Nxf154127e23cde0c8ecbaa8b943aff970c60c590f",
-                                "amount": 45,
-                                "fee": 5,
-                                "data": {}
-                            },
-                            "outputs": [
-                                {
-                                    "address": "Nxf9c62974d550c1f12cd7d6b9913b44983cb3a096",
-                                    "balance": 1000000000
-                                },
-                                {
-                                    "address": "Nxf154127e23cde0c8ecbaa8b943aff970c60c590f",
-                                    "balance": 45
-                                },
-                                {
-                                    "address": "miner",
-                                    "balance": 5
-                                }
-                            ],
-                            "time": 1568688154
+                            "from": "Nxf9c62974d550c1f12cd7d6b9913b44983cb3a096",
+                            "to": "Nxf154127e23cde0c8ecbaa8b943aff970c60c590f",
+                            "amount": 95,
+                            "fee": 5,
+                            "data": {},
+                            "time": 1568901661
                         }
                     }
                 ],
                 "outputs": [
                     {
-                        "address": "Nxf9c62974d550c1f12cd7d6b9913b44983cb3a096",
-                        "balance": 1000000055
+                        "address": "Nxf154127e23cde0c8ecbaa8b943aff970c60c590f",
+                        "balance": 95
                     },
                     {
-                        "address": "Nxf154127e23cde0c8ecbaa8b943aff970c60c590f",
-                        "balance": 45
+                        "address": "Nxf9c62974d550c1f12cd7d6b9913b44983cb3a096",
+                        "balance": 1000000105
                     }
                 ],
-                "time": 1567760775,
-                "nonce": 7935
+                "time": 1568902856,
+                "nonce": 5237
             }
         }
 
